a12f2917b890781b45f1445cd6e25815a9ca8070
Dimitar Dimitrov <41971494+ippoboi@users.noreply.github.com>
2024-05-28 09:06:46 +0200

    Update s3.ts

--- src/pages/api/s3.ts
+++ src/pages/api/s3.ts
@@ -12,8 +12,8 @@ const generateFileName = (bytes = 32) =>
 const s3Client = new S3Client({
   region: process.env.NEXT_PUBLIC_AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.NEXT_PRIVATE_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.NEXT_PRIVATE_AWS_SECRET_ACCESS_KEY!,
   },
 });
 
